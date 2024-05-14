a0ceb77904be99d0b6e328706fe5511cf46da5e8
kandol10 <prabbi.kandola@hotmail.co.uk>
2024-05-14 11:31:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -108,6 +108,7 @@ def process():
     result = crew.kickoff()
     return jsonify(result)  # Return the result as JSON
 
+
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
